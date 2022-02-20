c9703a9a097925deb9a96db020ca0d69e4c75a4f
muinsaiyed <muinsaiyed@gmail.com>
2022-02-20 18:05:49 -0500

    adding additional files and readme

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 # cars
 
-Run Docker file
-docker run -it -d car-dealership
+
+## Description
+This application is created using Springboot, MYSQL and Docker to containerize the app. This app will launch locally on port 8085. In order to launch the application, Docker must be installed.
+
+## API Information
+There are two endpoints /cars and /cars/{id}. The id will be an integer from 1-4 to pull information back. 
+
+## Launch Instructions
+1. Navigate to your app directory
+
+2. Run Docker build using the following command
+```make build```
+
+3. Run Application Locally using the following command
+```make local-dev```
+
+4. If you want to stop the application 
+```make local-dev-down```
+
+IF MAKE FILE DOESN'T WORK:
+1. `docker build -t car-dealership .`
+2. `docker-compose up`
--- init/3CInsertData.sql
+++ init/3CInsertData.sql
@@ -17,3 +17,16 @@ VALUES      (1,
              "sedan",
              120000,
              5000); 
+             
+             
+             INSERT INTO `dealership`.`cars` (idcars,Make,Model,CarPackage,Color,Year,Category,Mileage,Price) 
+VALUES (2,"Honda","Civic","Base","Yellow",2019,"Sedan",32442,17000);
+
+INSERT INTO `dealership`.`cars` (idcars,Make,Model,CarPackage,Color,Year,Category,Mileage,Price) 
+VALUES (3,"Toyota","Highlander","Sport","White",2019,"SUV",10500,34000);
+
+INSERT INTO `dealership`.`cars` (idcars,Make,Model,CarPackage,Color,Year,Category,Mileage,Price) 
+VALUES (4,"Toyota","Camry","Base","Grey",2020,"Sedan",1002,29000);
+
+INSERT INTO `dealership`.`cars` (idcars,Make,Model,CarPackage,Color,Year,Category,Mileage,Price) 
+VALUES (5,"Nissan","Maxima","Sport","Black",2021,"Sedan",3200,45000);
